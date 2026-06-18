e5aabe0416d858b149f87aec819acec7375c23d6
Salmany16 <344901@student.mboutrecht.nl>
2026-06-18 11:27:08 +0200

    Configure allergenen view to match required screenshot layout

--- resources/views/allergenen/index.blade.php
+++ resources/views/allergenen/index.blade.php
@@ -25,38 +25,32 @@
             <meta http-equiv="refresh" content="3;url={{ route('allergeen.index') }}">
         @endif
 
-        <a href="{{ route('allergeen.create') }}" class="btn btn-primary my-3">Nieuwe Allergeen</a>
+        <a href="{{ route('allergeen.create') }}" class="btn btn-primary my-3">Nieuw Allergeen</a>
 
-            <table class="table table-striped table-bordered align-middle shadow-sm">
-                <thead>
-                    <tr>
-                        <th>Naam</th>
-                        <th>Omschrijving</th>
-                        <th class="text-center">Verwijder</th>
-                        <th class="text-center">Wijzig</th>
-                    </tr>
-                </thead>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Naam</th>
+                    <th>Omschrijving</th>
+                    <th>Verwijder</th>
+                </tr>
+            </thead>
             <tbody>
                 @forelse ($allergenen as $allergeen)
                     <tr>
                         <td>{{ $allergeen->Naam }}</td>
                         <td>{{ $allergeen->Omschrijving }}</td>
-                        <td class="text-center">
+                        <td>
                             <form action="{{ route('allergeen.destroy', $allergeen->Id) }}" method="POST" onsubmit="return confirm('Weet je zeker dat je dit allergeen wilt verwijderen?');">
                                 @csrf
                                 @method('DELETE')
-                                <button type="submit" class="btn btn-danger btn-sm">Verwijder</button>
-                            </form>
-                        </td>
-                        <td class="text-center">
-                            <form action="{{ route('allergeen.edit', $allergeen->Id) }}" method="GET">
-                                <button type="submit" class="btn btn-success btn-sm">Wijzig</button>
+                                <button type="submit" class="btn btn-danger btn-sm">Verwijderen</button>
                             </form>
                         </td>
                     </tr>
                 @empty
                     <tr>
-                        <td colspan="4">Geen allergenen beschikbaar</td>
+                        <td colspan="3">Geen allergenen beschikbaar</td>
                     </tr>
                 @endforelse
             </tbody>

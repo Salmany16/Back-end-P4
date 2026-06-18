a9ad3800afdff3b605b8a153df71c7574fba9933
Salmany16 <344901@student.mboutrecht.nl>
2026-06-18 11:29:24 +0200

    Restore Wijzig column and styling in allergenen view to match instructions

--- resources/views/allergenen/edit.blade.php
+++ resources/views/allergenen/edit.blade.php
@@ -1,10 +1,10 @@
-@vite(['resources/css/app.css', 'resources/js/app.js'])
 <!DOCTYPE html>
 <html lang="nl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Jamin</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
     <div class="container d-flex justify-content-center mt-5">
--- resources/views/allergenen/index.blade.php
+++ resources/views/allergenen/index.blade.php
@@ -25,14 +25,15 @@
             <meta http-equiv="refresh" content="3;url={{ route('allergeen.index') }}">
         @endif
 
-        <a href="{{ route('allergeen.create') }}" class="btn btn-primary my-3">Nieuw Allergeen</a>
+        <a href="{{ route('allergeen.create') }}" class="btn btn-primary my-3">Nieuwe Allergeen</a>
 
-        <table class="table">
+        <table class="table table-striped table-bordered align-middle shadow-sm">
             <thead>
                 <tr>
                     <th>Naam</th>
                     <th>Omschrijving</th>
-                    <th>Verwijder</th>
+                    <th class="text-center">Verwijder</th>
+                    <th class="text-center">Wijzig</th>
                 </tr>
             </thead>
             <tbody>
@@ -40,17 +41,22 @@
                     <tr>
                         <td>{{ $allergeen->Naam }}</td>
                         <td>{{ $allergeen->Omschrijving }}</td>
-                        <td>
+                        <td class="text-center">
                             <form action="{{ route('allergeen.destroy', $allergeen->Id) }}" method="POST" onsubmit="return confirm('Weet je zeker dat je dit allergeen wilt verwijderen?');">
                                 @csrf
                                 @method('DELETE')
-                                <button type="submit" class="btn btn-danger btn-sm">Verwijderen</button>
+                                <button type="submit" class="btn btn-danger btn-sm">Verwijder</button>
+                            </form>
+                        </td>
+                        <td class="text-center">
+                            <form action="{{ route('allergeen.edit', $allergeen->Id) }}" method="GET">
+                                <button type="submit" class="btn btn-success btn-sm">Wijzig</button>
                             </form>
                         </td>
                     </tr>
                 @empty
                     <tr>
-                        <td colspan="3">Geen allergenen beschikbaar</td>
+                        <td colspan="4">Geen allergenen beschikbaar</td>
                     </tr>
                 @endforelse
             </tbody>
